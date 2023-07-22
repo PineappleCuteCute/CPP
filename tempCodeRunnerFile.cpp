7e86374ced58af0d807024ed71394a076b3245a4
ThanhManhCuteDangYeu <daothanhmanh12@gmail.com>
2023-07-22 10:47:42 +0700

    save

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,2 +1,21 @@
-yêu ngọc anh";
-}
+#include<iostream>
+using namespace std;
+
+int main(){
+    int x = 10;
+    int *p;
+    p = &x;
+    cout << (unsigned long int)&x << endl;
+    cout << (unsigned long int)p << endl;
+    cout << (unsigned long int)*p << endl;
+    cout << (unsigned long int)&p << endl;
+    cout << (unsigned long int)&*p << endl;
+
+
+    // int y = 100;
+    // int *q;
+    // q = &y;
+
+    // int *z;
+    // z = p - q;
+    // cout << *z;
